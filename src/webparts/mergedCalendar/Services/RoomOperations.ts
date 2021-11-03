1c1437815d4ed5c87995b7ab0f33299a8c204887
Maya-Mostafa <mayamostafa@gmail.com>
2021-11-03 13:40:01 -0400

    sorting rooms by sortOrder asc

--- src/webparts/mergedCalendar/Services/RoomOperations.ts
+++ src/webparts/mergedCalendar/Services/RoomOperations.ts
@@ -6,7 +6,7 @@ import * as moment from 'moment';
 
 export const getRooms = async (context: WebPartContext, roomsList: string) =>{
     console.log("Get Rooms Function");
-    const restUrl = context.pageContext.web.absoluteUrl + `/_api/web/lists/getByTitle('${roomsList}')/items`;
+    const restUrl = context.pageContext.web.absoluteUrl + `/_api/web/lists/getByTitle('${roomsList}')/items?$orderby=SortOrder asc`;
     const results = await context.spHttpClient.get(restUrl, SPHttpClient.configurations.v1).then(response => response.json());
 
     return results.value;
